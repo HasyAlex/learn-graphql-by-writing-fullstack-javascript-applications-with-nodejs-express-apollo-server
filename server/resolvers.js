4e982ca15c9dbe963748fe90ea9a46ca3f7d78a2
hasitha madusanka <hasithamadusankasl@.com>
2023-04-23 17:01:19 +0530

    delete and update jobs

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,4 @@
-import { getJobs, getJob, getJobsByCompany, createJob } from './db/jobs.js'
+import { getJobs, getJob, getJobsByCompany, createJob, deleteJob, updateJob } from './db/jobs.js'
 import { getCompany } from './db/companies.js'
 import { GraphQLError } from 'graphql';
 
@@ -26,6 +26,12 @@ export const resolvers = {
             const companyId = "FjcJCHJALA4i";
             return createJob({ companyId, title , description})
         },
+        deleteJob: (_root, {id}) => {
+            return deleteJob(id)
+        },
+        updateJob: (_root, {id, title, description}) => {
+            return updateJob({id, title, description})
+        }
     },
 
     //resolver function
